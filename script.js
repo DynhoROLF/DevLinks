99f2de9fc9adc9e80d6b3f5bacd5e050118482f3
Waldyr Filho <54288561+WaldyrFilho@users.noreply.github.com>
2023-07-01 15:02:41 -0300

    update: more professional

--- script.js
+++ script.js
@@ -20,14 +20,4 @@ function toggleMode() {
       'Foto de Waldyr com uniforme da Lufa Lufa e fundo preto.'
     )
   }
-
-  const rocket = document.querySelector('#social-links img')
-  if (html.classList.contains('light')) {
-    // se tiver light mode, adicionar a imagem light
-    rocket.setAttribute('src', './assets/rocketseat.png')
-  } else {
-    // se tiver sem light mode, manter a imagem normal
-    rocket.setAttribute('src', './assets/rocketseatWhite.png')
-  }
-
 }
